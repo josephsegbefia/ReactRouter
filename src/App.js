2b35a54e1e260b281a900b42af617eca07ae959c
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-06-17 09:55:19 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createBrowserHistory } from "history";
+const history = createBrowserHistory();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Route = ({ path, component }) => {
+  const pathname = window.location.pathname;
+  if (pathname.match(path)) {
+    return React.createElement(component);
+  } else {
+    return null;
+  }
+};
+
+const Link = ({ to, children }) => {
+  <a
+    onClick={(e) => {
+      e.preventDefault();
+      history.push(to);
+    }}
+  >
+    {children}
+  </a>;
+};
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h2>Which body of water?</h2>
+        <ul>
+          <li>
+            <Link to="/atlantic">
+              <code>/atlantic</code>
+            </Link>
+          </li>
+          <li>
+            <Link to="/pacific">
+              <code>/pacific</code>
+            </Link>
+          </li>
+        </ul>
+        <hr />
+        <Route path="/atlantic" component={Atlantic} />
+        <Route path="/pacific" component={Pacific} />
+      </div>
+    );
+  }
 }
+
+const Atlantic = () => (
+  <div>
+    <h3>Atlantic Ocean</h3>
+    <p>
+      The Atlantic Ocean covers approximately 1/5th of the surface of the earth
+    </p>
+  </div>
+);
+
+const Pacific = () => (
+  <div>
+    <h3>Pacific Ocean</h3>
+    <p>
+      Ferdinand Magellan, a Portuguese explorer, named the ocean 'mar pacifico'
+      in 1521, which means peaceful sea.
+    </p>
+  </div>
+);
 
 export default App;
